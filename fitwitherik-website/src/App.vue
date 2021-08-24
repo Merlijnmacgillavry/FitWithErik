ae0d250a80274f7d7531d42307168c35ef9a51c5
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-08-24 22:36:12 +0200

    Added HomeComponent

--- fitwitherik-website/src/App.vue
+++ fitwitherik-website/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content">
+    <div class="navbar"></div>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/home.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Home },
+};
 </script>
 
 <style lang=scss>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ fitwitherik-website/src/components/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="home">Hello world!</section>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
