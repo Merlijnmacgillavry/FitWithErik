a5ca375fdfe7624c2dec556261a6db70ad84e352
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-08-26 00:07:21 +0200

    Added Navbar prototype
    Finished mobile Navbar
    WIP: desktop Navbar

--- fitwitherik-website/src/App.vue
+++ fitwitherik-website/src/App.vue
@@ -14,13 +14,29 @@ export default {
 };
 </script>
 
-<style lang=scss>
+<style lang=scss >
+@import url("https://fonts.googleapis.com/css2?family=Big+Shoulders+Text:wght@100;300;400;500;600;700;800;900&display=swap");
+
+@import "./_config.scss";
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
+
+@mixin transition-ease {
+  transition: all 0.5s ease-in-out;
+}
 body {
-  background: linear-gradient(90deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9));
+  font-family: "Big Shoulders Text";
+  background: lighten($background-color, $amount: 10);
+  color: $secondary-color;
+}
+h1 {
+  max-width: 80rem;
+  font-size: 3rem;
+  border-bottom: 4px solid $secondary-color;
+  font-family: "Big Shoulders Text", bold 700;
+  cursor: pointer;
 }
 </style>
--- fitwitherik-website/src/components/Navbar.vue
+++ fitwitherik-website/src/components/Navbar.vue
@@ -1,72 +1,215 @@
 <template>
-  <nav class="navbar">
-    <div class="nav-icon">
-      <img src="../assets/logo.png" alt="" />
+  <div>
+    <div class="menu-btn" v-on:click="toggleMenu">
+      <span class="menu-btn__burger" ref="hamburger"></span>
     </div>
-    <ul class="menu-nav">
-      <li class="menu-nav__item">
-        <a href="#!" class="menu-nav__link">Home</a>
-      </li>
-      <li class="menu-nav__item">
-        <a href="#!" class="menu-nav__link">Online Coaching</a>
-      </li>
-      <li class="menu-nav__item">
-        <a href="#!" class="menu-nav__link">Custom Plans</a>
-      </li>
-      <li class="menu-nav__item">
-        <a href="#!" class="menu-nav__link">Recipes</a>
-      </li>
-      <li class="menu-nav__item">
-        <a href="#!" class="menu-nav__link">About me</a>
-      </li>
-      <li class="menu-nav__item">
-        <a href="#!" class="menu-nav__link">Contact</a>
-      </li>
-    </ul>
-  </nav>
+
+    <nav class="nav" ref="nav">
+      <ul class="menu-nav" ref="menuNav">
+        <li class="menu-nav__item">
+          <h1 class="menu-nav__link">FIT WITH ERIK COACHING</h1>
+        </li>
+        <li
+          class="menu-nav__item"
+          v-for="({ name }, index) in links"
+          :key="index"
+        >
+          <a href="#!" class="menu-nav__link">{{ name }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Navbar",
+  data: function () {
+    return {
+      showMenu: false,
+      links: [
+        {
+          name: "Home",
+        },
+        {
+          name: "Online Coaching",
+        },
+        {
+          name: "Custom Plans",
+        },
+        {
+          name: "Recipes",
+        },
+        {
+          name: "About me",
+        },
+        {
+          name: "Contact",
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleMenu: function () {
+      let hamburger = this.$refs.hamburger;
+      let nav = this.$refs.nav;
+      let navItems = this.$el.querySelectorAll(".menu-nav__item");
+      let menuNav = this.$refs.menuNav;
+      if (!this.showMenu) {
+        hamburger.classList.add("open");
+        nav.classList.add("open");
+        menuNav.classList.add("open");
+        navItems.forEach((item) => item.classList.add("open"));
+      } else {
+        hamburger.classList.remove("open");
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+        navItems.forEach((item) => item.classList.remove("open"));
+      }
+      this.showMenu = !this.showMenu;
+    },
+  },
+  mounted: function () {},
 };
 </script>
 
 <style lang="scss" scoped>
-.navbar {
-  position: fixed;
-  top: 0%;
-  left: 0%;
-  width: 100vw;
-  opacity: 0.98;
+@import "../_config.scss";
+.menu-btn {
+  position: absolute;
   z-index: 1;
-  display: flex;
-  justify-content: space-between;
-  background: pink;
-  .nav-icon {
-    padding-left: 1rem;
-    display: flex;
-    align-items: center;
-    img {
-      max-width: 100%;
-      max-height: 60px;
-      cursor: pointer;
+  right: 1rem;
+  top: 1rem;
+  height: 20px;
+  width: 28px;
+  cursor: pointer;
+  @include transition-ease;
+
+  &__burger {
+    position: absolute;
+    right: 0;
+    top: 0.5rem;
+    width: 28px;
+    height: 3px;
+    background: white;
+    @include transition-ease;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: -8px;
+      width: 28px;
+      height: 3px;
+      background: white;
+      @include transition-ease;
+    }
+    &::after {
+      content: "";
+      position: absolute;
+      top: 8px;
+      width: 20px;
+      height: 3px;
+      background: white;
+      @include transition-ease;
+    }
+    &.open {
+      transform: rotate(720deg);
+      background: transparent;
+      &::before {
+        transform: rotate(45deg) translate(5px, 8px);
+      }
+      &::after {
+        width: 28px;
+        transform: rotate(-45deg) translate(3px, -7px);
+      }
     }
   }
+}
+.nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  opacity: 0.98;
+  visibility: hidden;
+
+  &.open {
+    visibility: visible;
+  }
   .menu-nav {
-    display: block;
-    height: 100%;
-    text-align: right;
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    overflow: hidden;
+    background: $background-color;
     list-style-type: none;
     padding-right: 1rem;
+    transform: translateY(-100%);
+    @include transition-ease;
+
+    &.open {
+      transform: translateY(0);
+    }
+
     &__item {
-      display: inline;
-      padding-right: 1.5rem;
+      transform: translateX(100vw);
+      @include transition-ease;
+      &.open {
+        transform: translateX(0);
+      }
+      &.active > a {
+        color: white;
+      }
     }
     &__link {
       display: inline-block;
-      padding: 1rem 0;
-      font-size: 1.3em;
+      font-size: 2rem;
+      text-transform: uppercase;
+      padding: 2rem 0;
+      font-weight: 300;
+      @include transition-ease;
+
+      &:hover {
+        color: white;
+      }
+    }
+    h1 {
+      padding: 0%;
+    }
+  }
+
+  // Automate with vue
+
+  @for $i from 1 through 8 {
+    .menu-nav__item:nth-child(#{$i}) {
+      transition-delay: ($i * 0.1s) + 0.15s;
+    }
+  }
+}
+@include media-md {
+  .menu-btn {
+    visibility: hidden;
+  }
+  .nav {
+    visibility: visible;
+    .menu-nav {
+      display: block;
+      transform: translateY(0);
+      height: 100%;
+      background: gradient(to-right, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.85));
+      text-align: center;
+      h1 {
+        display: none;
+      }
+      &__item {
+        display: inline;
+        padding-right: 1.5rem;
+      }
+      &__link {
+        font-size: 1.5rem;
+      }
     }
   }
 }
