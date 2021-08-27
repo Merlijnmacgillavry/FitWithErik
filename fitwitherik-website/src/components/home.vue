12a6fb14ea201885bb1df951b1f52ad4cededa75
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-08-28 00:58:15 +0200

    Revert "HomePage added"
    This reverts commit 169e2f74de60a0cb25815500bb12cc66322e39cf.

--- fitwitherik-website/src/components/home.vue
+++ fitwitherik-website/src/components/home.vue
@@ -1,16 +1,5 @@
 <template>
-  <section class="home">
-    <div class="home__content">
-      <h1 class="home__content__title">FIT WITH ERIK COACHING</h1>
-      <p class="home__content__tag-line">
-        Helps you lose fat, build muscle and feel great WITHOUT giving up your
-        personal life!
-      </p>
-      <div class="home__content__btn">
-        <button class="btn">CHANGE YOUR LIFE</button>
-      </div>
-    </div>
-  </section>
+  <section class="home">Hello world!</section>
 </template>
 
 <script>
@@ -20,45 +9,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "../_config.scss";
-
-.home {
-  background: linear-gradient(
-      to bottom right,
-      rgba($background-color, 0.6),
-      rgba($background-color, 0.95)
-    ),
-    url("../assets/home.jpeg");
-  min-height: 100vh;
-  background-size: cover;
-  background-position: center top;
-
-  .home__content {
-    padding: 40% 5% 0 5%;
-    font-family: "Bebas Neue";
-    &__title {
-      text-align: center;
-      color: $secondary-color;
-      border: none;
-      font-size: 3.8rem;
-    }
-    &__tag-line {
-      font-family: "Big Shoulders Text", bold 500;
-      text-align: center;
-      font-size: 2rem;
-      text-transform: uppercase;
-    }
-    &__btn {
-      text-align: center;
-    }
-  }
-}
-
-@include media-md {
-  .home {
-    .home__content {
-      padding-top: 25%;
-    }
-  }
-}
 </style>
