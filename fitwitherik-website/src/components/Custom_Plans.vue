63e6af436577d6880d4f03583512d4ab53292979
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-08-31 23:45:52 +0200

    resolves #6

--- fitwitherik-website/src/components/Custom_Plans.vue
+++ fitwitherik-website/src/components/Custom_Plans.vue
@@ -2,10 +2,10 @@
   <section class="custom-plans">
     <h2 class="custom-plans__title">Custom Plans</h2>
     <div class="custom-plans__card-container">
-      <div class="card">
-        <div v-if="!cards[0].flipped" class="front">
-          <img src="../assets/plans_1.png" alt="" />
-          <div class="content">
+      <transition name="flip" mode="out-in">
+        <div v-if="!cards[0].flipped" key="front" class="card">
+          <img class="front-img" src="../assets/plans_1.png" alt="" />
+          <div class="front-content">
             <h3>MUSCLE MAXIMIZATION</h3>
             <p>Training plan</p>
           </div>
@@ -13,10 +13,9 @@
             MORE INFO
           </button>
         </div>
-
-        <div v-if="cards[0].flipped" class="back">
-          <h3 class="title">THE MUSCLE MAXIMIZATION PLAN</h3>
-          <ul class="items">
+        <div v-else key="back" class="card">
+          <h3 class="back-title">THE MUSCLE MAXIMIZATION PLAN</h3>
+          <ul class="back-items">
             <li>A 16-week personalized muscle building training program</li>
             <li>Adjusted to your needs every 4 weeks</li>
             <li>Access to my coaching app</li>
@@ -26,11 +25,11 @@
             LESS INFO
           </button>
         </div>
-      </div>
-      <div class="card">
-        <div v-if="!cards[1].flipped" class="front">
-          <img src="../assets/plans_2.png" alt="" />
-          <div class="content">
+      </transition>
+      <transition name="flip" mode="out-in" key="front">
+        <div v-if="!cards[1].flipped" class="card">
+          <img class="front-img" src="../assets/plans_2.png" alt="" />
+          <div class="front-content">
             <h3>FATLOSS FATALITY</h3>
             <p>Diet plan</p>
           </div>
@@ -38,10 +37,9 @@
             MORE INFO
           </button>
         </div>
-
-        <div v-if="cards[1].flipped" class="back">
-          <h3 class="title">THE FATLOSS FATALITY DIET PLAN</h3>
-          <ul class="items">
+        <div class="card" v-else key="back">
+          <h3 class="back-title">THE FATLOSS FATALITY DIET PLAN</h3>
+          <ul class="back-items">
             <li>A 16-week personalized fat burning diet plan</li>
             <li>Adjusted to your needs every 2 weeks</li>
             <li>Access to my coaching app</li>
@@ -51,7 +49,7 @@
             LESS INFO
           </button>
         </div>
-      </div>
+      </transition>
     </div>
   </section>
 </template>
@@ -105,68 +103,48 @@ export default {
       height: 600px;
       width: 400px;
       margin: 2em auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
       webkit-box-shadow: 9px 10px 22px -8px rgba($background-color, 0.5);
       -moz-box-shadow: 9px 10px 22px -8px rgba($background-color, 0.5);
       box-shadow: 9px 10px 22px -8px rgba($background-color, 0.5);
+
       .btn {
         margin: auto 1em 1em 1em;
       }
-      .front {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        img {
-          width: 100%;
-          height: 357x;
-          border-bottom: 3px solid $primary-color;
-          border-top-left-radius: 4px;
-          border-top-right-radius: 4px;
+      .front-img {
+        width: 100%;
+        height: 357x;
+        border-bottom: 3px solid $primary-color;
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+      }
+      .front-content {
+        padding: 1em 1em;
+        h3 {
+          font-size: 2em;
         }
-        .content {
-          padding: 1em 1em;
-          h3 {
-            font-size: 2em;
-          }
-          p {
-            font-size: 1.5em;
-          }
+        p {
+          font-size: 1.5em;
         }
       }
-      .back {
-        height: 100%;
-        display: flex;
-        justify-content: space-between;
-        flex-direction: column;
-        .title {
-          padding: 1em;
-          font-size: 2em;
-          text-decoration: underline;
-          text-decoration-color: $primary-color;
-        }
-        .items {
-          padding: 0 4em;
-          text-align: left;
-          list-style-type: square;
-          li {
-            font-size: 1.5em;
-          }
+
+      .back-title {
+        padding: 1em;
+        font-size: 2em;
+        text-decoration: underline;
+        text-decoration-color: $primary-color;
+      }
+      .back-items {
+        padding: 0 4em;
+        text-align: left;
+        list-style-type: square;
+        li {
+          font-size: 1.5em;
         }
       }
     }
-  }
-  .flip-enter-active {
-    transition: all 0.4s ease;
-  }
-
-  .flip-leave-active {
-    display: none;
-  }
-
-  .flip-enter,
-  .flip-leave {
-    transform: rotateY(180deg);
-    opacity: 0;
   }
 }
 @include media-md {
