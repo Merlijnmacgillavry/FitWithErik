fa7ef63ab07e8066b484dd3650de9fbc27a5e724
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-09-12 17:21:20 +0200

    resolves #68

--- fitwitherik-website/src/components/Thank_you.vue
+++ fitwitherik-website/src/components/Thank_you.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="thank_you">
     <h2 class="thank_you__title">Thank You!</h2>
-    <h3 class="thank_you__message">
-      {{
-        $route.params.type === "apply"
-          ? "Thank you for submitting your application!"
-          : "Thank you for submitting your question!"
-      }}
+    <h3 v-if="$route.params.type === 'apply'" class="thank_you__message">
+      Thank you for submitting your application!
+      <br />
+      Please schedule a meeting with me to further work out your goals and needs
     </h3>
+    <h3 v-else>Thank you for submitting your question!</h3>
+    <div
+      v-if="$route.params.type === 'apply'"
+      class="calendly-inline-widget"
+      data-url="https://calendly.com/fitwitherik?background_color=0f0f0f&text_color=ffffff&primary_color=ff5f1f"
+      style="min-width: 320px; height: 700px"
+    ></div>
     <router-link to="/">
       <button class="btn">GO BACK TO THE SITE</button>
     </router-link>
@@ -38,9 +43,17 @@ export default {
   &__message {
     font-size: 1.2rem;
     margin: 1em;
+    color: $primary-color;
   }
   .btn {
     margin-top: 2em;
+  }
+  .calendly-inline-widget {
+    height: 100%;
+    * {
+      margin-top: 0 !important;
+      margin-bottom: 0 !important;
+    }
   }
 }
 @include media-md {
