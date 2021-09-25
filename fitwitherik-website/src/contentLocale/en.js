ac5cfcba443c67e7fcb0ed21ab5cfb751f05e543
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-09-25 15:54:41 +0200

    recipes translated

--- fitwitherik-website/src/contentLocale/en.js
+++ fitwitherik-website/src/contentLocale/en.js
@@ -1,8 +1,9 @@
 import home from "./home/home-en"
 import onlineCoaching from "./online-coaching/online-coaching-en"
+import recipes from "./recipes/recipes-en"
 const en = {
     home: home,
-    onlineCoaching: onlineCoaching
-
+    onlineCoaching: onlineCoaching,
+    recipes: recipes
 }
 export default en
--- fitwitherik-website/src/contentLocale/nl.js
+++ fitwitherik-website/src/contentLocale/nl.js
@@ -1,7 +1,9 @@
 import home from "./home/home-nl"
 import onlineCoaching from "./online-coaching/online-coaching-nl";
+import recipes from "./recipes/recipes-nl";
 const nl = {
     home: home,
-    onlineCoaching: onlineCoaching
+    onlineCoaching: onlineCoaching,
+    recipes: recipes
 }
 export default nl;
