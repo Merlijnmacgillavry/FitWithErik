ef5928585cc5d609c21b2c7073e790e86fb57e68
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-09-26 17:45:55 +0200

    translated navbar

---
+++ fitwitherik-website/src/contentLocale/navbar/navbar-en.js
@@ -0,0 +1,29 @@
+const navbar = {
+    links: [
+        {
+            name: "Online Coaching",
+            path: "/online_coaching",
+        },
+        {
+            name: "Custom Plans",
+            path: "/custom_plans",
+        },
+        {
+            name: "Recipes",
+            path: "/recipes",
+        },
+        {
+            name: "About me",
+            path: "/about",
+        },
+        {
+            name: "Contact",
+            path: "/contact",
+        },
+        {
+            name: "Testimonials",
+            path: "/testimonials",
+        },
+    ],
+}
+export default navbar
