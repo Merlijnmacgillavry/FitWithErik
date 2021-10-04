611c4f67f0fc32090325cff163507b1bb9451223
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-10-04 22:20:34 +0200

    resolves #92

--- fitwitherik-website/src/contentLocale/contact/contact-nl.js
+++ fitwitherik-website/src/contentLocale/contact/contact-nl.js
@@ -2,7 +2,7 @@ const contact = {
     title: 'Contact',
     text_1: 'Voor algemene vragen over coaching, plannen, recepten of andere, kies ',
     text_2: '. Als je online coaching wilt aanvragen of met mij wilt praten over trajecten, kies dan',
-    text_3: '. U wordt doorgestuurd naar de juiste pagina waar u de benodigde informatie kunt invullen.',
+    text_3: '. Je wordt doorgestuurd naar de juiste pagina waar je de benodigde informatie kunt invullen.',
     button_1: 'NEEM CONTACT OP',
     button_2: 'NU AANMELDEN',
 }
