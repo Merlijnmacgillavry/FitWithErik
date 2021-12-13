13912b6bed564d286dca97640de2f3c78ffd7488
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2021-12-13 13:12:39 +0100

    resolves #105

--- fitwitherik-website/src/contentLocale/footer/footer-nl.js
+++ fitwitherik-website/src/contentLocale/footer/footer-nl.js
@@ -1,5 +1,5 @@
 const footer = {
-    text: "We gebruiken cookies om ervoor te zorgen dat onze website compleet functioneert. Als je doorgaat met het gebruiken van de website, gaan we er vanuit dat ermee instemt.",
+    text: "We gebruiken cookies om ervoor te zorgen dat onze website compleet functioneert. Als je doorgaat met het gebruiken van de website, gaan we er vanuit dat er mee instemt.",
     button: "Oké!"
 }
 export default footer;
